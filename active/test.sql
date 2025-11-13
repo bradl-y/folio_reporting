53eac975da85bd92177b44b7137fea62a1e44f9a
bradl-y <bcumming@nla.gov.au>
2025-11-13 20:51:00 +1100

    test

--- active/test.sql
+++ active/test.sql
@@ -3,22 +3,24 @@
 DROP FUNCTION IF EXISTS bcumming.test()
 
 CREATE FUNCTION bcumming.test()
-RETURNS TABLE(
-    barcode text
-) AS
-$$
-BEGIN
-    BEGIN
-        RETURN QUERY
-            select
-                u.barcode
-            from
-                folio_users.users__t u
-            where
-                u.id = 'c3d090d0-e1bc-5a66-a54f-e029b66a2446';
-    END;
-END;
-$$
-LANGUAGE plpgsql
-STABLE
-PARALLEL SAFE;
+ RETURNS character varying
+ LANGUAGE plpgsql
+AS $function$
+declare
+thisUserName varchar = '';
+
+begin
+	select
+	username
+into
+	thisUserName
+from
+	folio_users.users__t ut
+where
+	ut.id = 'c3d090d0-e1bc-5a66-a54f-e029b66a2446';
+
+return thisUserName;
+end;
+
+$function$
+;
