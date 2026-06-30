329d0189ec0de6470c1263db6d0484f13cc01630
bradl-y <bcumming@nla.gov.au>
2026-07-01 08:45:45 +1000

    Updated loan history query

--- active/get_patron_loan_history.sql
+++ active/get_patron_loan_history.sql
@@ -22,8 +22,8 @@ BEGIN
 				concat_ws(' ', u.jsonb->'personal'->>'firstName', u.jsonb->'personal'->>'lastName') as patron_name,
 				li.loan_status as loan_status,
 				ihi.title as title,
-				to_char(li.loan_date, 'DD-MM-YYYY HH:MI am') as loan_date,
-				to_char(li.loan_return_date, 'DD-MM-YYYY HH:MI am') as loan_return_date,
+                to_char((li.loan_date::timestamp AT TIME ZONE 'UTC' AT TIME ZONE 'Australia/Canberra'), 'DD-MM-YYYY HH:MI am') as loan_date,
+				to_char((li.loan_return_date::timestamp AT TIME ZONE 'UTC' AT TIME ZONE 'Australia/Canberra'), 'DD-MM-YYYY HH:MI am') as loan_return_date,
 				li.barcode as item_barcode,
 				ihi.call_number as call_number
 			from
